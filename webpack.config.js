0aeddc9c9418c2352df5a9c917fa9bae13b31d08
Joni Kämppä <joni.kamppa@sc5.io>
2017-10-04 14:17:02 +0300

    upadte webpack configuration, use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
-const path = require('path');
+let webpack = require('webpack');
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const { dependencies } = require('./package.json');
+let VENDOR_LIBS = [];
+Object.entries(dependencies).forEach(([key, value]) => {
+  VENDOR_LIBS.push(key);
+});
 
 module.exports = {
-  context: path.join(__dirname, 'app'),
-  entry: ['./main.js',
-  ],
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
       {
+        use: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        exclude: /node_modules/
       },
-    ],
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
   },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
