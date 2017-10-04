fa02d10b3e1462533370452f92b99f1feaae2e29
Joni Kämppä <joni.kamppa@sc5.io>
2017-10-04 11:13:05 +0300

    add react

--- app/main.js
+++ app/main.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './counter';
+
 console.log("Hello world!");
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
